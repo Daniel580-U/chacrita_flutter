22903e028b90bfcfcfb075b6265a47704922f491
Jackson Fernando Merma Portocarrero <jmermap@unsa.edu.pe>
2024-11-13 20:23:10 -0500

    feat: calling to extractFrames method with a specific case file
    Also, I've changed the minSdk version

--- lib/features/main_features/record/pages/record_page.dart
+++ lib/features/main_features/record/pages/record_page.dart
@@ -1,7 +1,44 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class RecordPage extends StatelessWidget {
+import 'package:chacrita/features/main_features/record/controllers/record_controller.dart';
+import 'package:chacrita/features/main_features/record/services/record_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class RecordPage extends StatefulWidget {
   const RecordPage({Key? key}) : super(key: key);
+
+  @override
+  _RecordPageState createState() => _RecordPageState();
+}
+
+class _RecordPageState extends State<RecordPage> {
+  final RecordService _recordService = RecordService();
+  final RecordController _recordController = RecordController();
+  String _resultText = 'Bienvenido a record';
+
+  Future<void> _identifyPlant() async {
+    /*
+    final byteData = await rootBundle.load('assets/img/plant.jpg');
+    final tempDir = Directory.systemTemp;
+    final tempFile = File('${tempDir.path}/plant.jpg');
+    await tempFile.writeAsBytes(byteData.buffer.asUint8List());
+    //final File image = File('assets/img/plant.jpg');
+    final result = await _recordService.identifyPlant(tempFile);
+    */
+    setState(() {
+      //_resultText = result != null ? result.toString() : 'Identificación fallida';
+      _resultText = 'Procesando...';
+    });
+
+    List<File> imageFiles = await _recordController.extractFrames();
+
+    // Updating result state
+    setState(() {
+      //_resultText = result != null ? result.toString() : 'Identificación fallida';
+      _resultText = 'Imágenes guardadas';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +47,17 @@ class RecordPage extends StatelessWidget {
         title: const Text('record Page'),
       ),
       body: Center(
-        child: Text('Bienvenido a record'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_resultText),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _identifyPlant,
+              child: const Text('Identificar Planta'),
+            ),
+          ],
+        ),
       ),
     );
   }
