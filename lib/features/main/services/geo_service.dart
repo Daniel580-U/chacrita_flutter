e73d774b158130853e9ad1a3827f1351e8a745f6
Jackson Fernando Merma Portocarrero <jmermap@unsa.edu.pe>
2024-11-14 19:03:40 -0500

    refactor: passing weather request to the geo service in main view

---
+++ lib/features/main/services/geo_service.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+
+class GeoService {
+  Future<Map<String, dynamic>?> getWeatherRequest(double lat, double lan, String? apiUrl) async {
+    // Getting environment variables
+    String? apiKey = dotenv.env['OPEN_WEATHER_API_KEY'];
+
+    // Default config
+    final url = Uri.parse('$apiUrl?lat=$lat&lon=$lan&appid=$apiKey');
+
+    // Sending request
+    final response = await http.get(url);
+
+    // Request status
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      print('Error: ${response.statusCode}');
+      return null;
+    }
+  }
+}
--- lib/features/main_features/record/services/record_service.dart
+++ lib/features/main_features/record/services/record_service.dart
@@ -5,7 +5,6 @@ import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
 
 class RecordService {
-
   Future<Map<String, dynamic>?> identifyPlant(File imageFile) async {
 
     // Getting environment variables
@@ -32,25 +31,6 @@ class RecordService {
     if (response.statusCode == 200) {
       final body = await response.stream.bytesToString();
       return jsonDecode(body);
-    } else {
-      print('Error: ${response.statusCode}');
-      return null;
-    }
-  }
-
-  Future<Map<String, dynamic>?> getWeatherRequest(double lat, double lan, String? apiUrl) async {
-    // Getting environment variables
-    String? apiKey = dotenv.env['OPEN_WEATHER_API_KEY'];
-
-    // Default config
-    final url = Uri.parse('$apiUrl?lat=$lat&lon=$lan&appid=$apiKey');
-
-    // Sending request
-    final response = await http.get(url);
-
-    // Request status
-    if (response.statusCode == 200) {
-      return jsonDecode(response.body);
     } else {
       print('Error: ${response.statusCode}');
       return null;
