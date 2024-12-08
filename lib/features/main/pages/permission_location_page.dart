d5b5e5d9945fbc972579c863eb1f78dbd9df0329
Daniel <dbedregalp@unsa.edu.pe>
2024-12-08 15:04:39 -0500

    feat: integrate api's services and controllers with pages

--- lib/features/main/pages/permission_location_page.dart
+++ lib/features/main/pages/permission_location_page.dart
@@ -1,6 +1,11 @@
 import 'package:chacrita/features/main/pages/main_page.dart';
+import 'package:chacrita/features/main/services/geo_service.dart';
+import 'package:chacrita/provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:provider/provider.dart';
+import 'package:chacrita/features/main/controllers/geolocator_controller.dart';
 
 class LocationPermissionPage extends StatefulWidget {
   const LocationPermissionPage({super.key});
@@ -11,6 +16,9 @@ class LocationPermissionPage extends StatefulWidget {
 
 class _LocationPermissionPageState extends State<LocationPermissionPage> {
   bool _isPermissionGranted = false;
+  Map <String, dynamic>? apiWeather;
+  Map <String, dynamic>? apiForecast;
+  Map <String, dynamic>? apiAirPollution;
   @override
   void initState() {
     super.initState();
@@ -23,6 +31,22 @@ class _LocationPermissionPageState extends State<LocationPermissionPage> {
 
     if (status.isGranted) {
       // Si el permiso es concedido, navega a la siguiente pantalla
+      GeolocatorController().getCurrentLocation().then((position) async{
+        if (position != null) {
+          double lat = position.latitude;
+          double lon = position.longitude;
+
+          // Open weather experiment
+          String? weatherApiUrl = dotenv.env['OPEN_WEATHER_WEATHER_ENDPOINT'];
+          String? forecastApiUrl = dotenv.env['OPEN_WEATHER_FORECAST_ENDPOINT'];
+          String? airPollutionApiUrl = dotenv.env['OPEN_WEATHER_AIR_POLLUTION_ENDPOINT'];
+          apiWeather= await GeoService().getApiRequest(lat, lon, weatherApiUrl);
+          apiForecast= await GeoService().getApiRequest(lat, lon,forecastApiUrl);
+          apiAirPollution= await GeoService().getApiRequest(lat, lon, airPollutionApiUrl);
+          
+          
+        }
+      });
       setState(() {
         _isPermissionGranted = true;
       });
@@ -41,7 +65,24 @@ class _LocationPermissionPageState extends State<LocationPermissionPage> {
 
   @override
   Widget build(BuildContext context) {
+    final globalState = Provider.of<GlobalState>(context);
     if (_isPermissionGranted) {
+      if (globalState.apiWeather == null && apiWeather != null) {
+        globalState.updateApiWeather(apiWeather!);
+      } else {
+        print('Error: No se pudo obtener la información del clima');
+      }
+      if (globalState.apiForecast == null && apiForecast != null) {
+        globalState.updateApiForecast(apiForecast!);
+      } else {
+        print('Error: No se pudo obtener la información del pronóstico');
+      }
+      if (globalState.apiAirPollution == null && apiAirPollution != null) {
+        globalState.updateApiAirPollution(apiAirPollution!);
+      } else {
+        print('Error: No se pudo obtener la información de la contaminación del aire');
+      }
+      
       return const MainPage();
     } else {
       return Scaffold(
--- lib/features/main_features/record/pages/record_page.dart
+++ lib/features/main_features/record/pages/record_page.dart
@@ -1,9 +1,12 @@
 
 import 'dart:io';
 
+import 'package:chacrita/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:chacrita/features/main_features/record/services/record_service.dart';
+import 'package:provider/provider.dart';
 
 
 class RecordPage extends StatefulWidget {
@@ -16,13 +19,13 @@ class RecordPage extends StatefulWidget {
 class _CaptureScreenState extends State<RecordPage> {
   final ImagePicker _picker = ImagePicker();
   XFile? _image;
+  
 
   @override
   void initState() {
     super.initState();
-    _checkCameraPermission();
   }
-  Future<void> _checkCameraPermission() async {
+  Future<void> _checkCameraPermission(BuildContext context) async {
     var status = await Permission.camera.status;
     if (status.isDenied) {
       // Solicita permiso si está denegado
@@ -30,7 +33,13 @@ class _CaptureScreenState extends State<RecordPage> {
     }
     if (status.isGranted) {
       // Si el permiso es concedido, abre la cámara
-      _takePicture();
+      _takePicture(context);
+      
+      if(_image != null) {
+        
+      }else{
+        print("No se ha capturado ninguna foto");
+      }
     } else {
       // Muestra mensaje si el permiso es denegado
       Navigator.of(context).pop('/main');
@@ -39,33 +48,34 @@ class _CaptureScreenState extends State<RecordPage> {
       );
     }
   }
-
-  Future<void> _takePicture() async {
+  Future<void> _takePicture(BuildContext context) async {
     final XFile? photo = await _picker.pickImage(source: ImageSource.camera);
+    final GlobalState globalState = Provider.of<GlobalState>(context, listen: false);
     if (photo != null) {
       setState(() {
         _image = photo;
       });
+      Map<String,dynamic>? apiPhoto= await RecordService().identifyPlant(File(_image!.path));
+        if(apiPhoto != null){
+          globalState.updateApiPhoto(apiPhoto);
+        }
+      Navigator.of(context).pop('/main');
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    if(_image == null) _checkCameraPermission(context);
     return Scaffold(
       appBar: AppBar(title: const Text('Captura de Foto')),
-      body: Center(
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            _image == null
-                ? const Text("No se ha capturado ninguna foto")
-                : Image.file(File(_image!.path)),
+                Center(child: CircularProgressIndicator()),
+
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _checkCameraPermission,
-              child: const Text("Tomar Foto"),
-            ),
-          ],
+            ],
         ),
       ),
     );
