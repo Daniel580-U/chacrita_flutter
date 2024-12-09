d24c3892c24db3617f16e29821b150b830ae2a2b
Daniel <dbedregalp@unsa.edu.pe>
2024-12-08 19:24:54 -0500

    style: changing the button styles

--- lib/features/main/pages/main_page.dart
+++ lib/features/main/pages/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:chacrita/app/config/colors.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatelessWidget {
@@ -20,7 +21,7 @@ class MainPage extends StatelessWidget {
             child: Center(
               child: Column(
                 children: [
-                  const SizedBox(height: 100),
+                  const SizedBox(height: 50),
                   const Text(
                     "CHAKRITA",
                     style: TextStyle(
@@ -31,12 +32,13 @@ class MainPage extends StatelessWidget {
                         color: Colors.white),
                     selectionColor: Colors.white,
                   ),
-                  const SizedBox(height: 100),
+                  const SizedBox(height: 15),
                   Image.asset(
-                    'assets/img/chacrita_logo.png',
-                    width: 151,
-                    height: 152,
+                    'assets/img/chacrita_white.png',
+                    width: 200,
+                    height: 200,
                   ),
+                  const SizedBox(height: 100),
                 ],
               ),
             ),
@@ -46,43 +48,104 @@ class MainPage extends StatelessWidget {
             // Clipper para crear la curvatura
             child: Container(
                 height: MediaQuery.of(context).size.height *
-                    0.4, // 80% de la altura
+                    0.55, // 80% de la altura
                 color: Colors.white,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                child: Column(
                   children: [
-                    ElevatedButton(
-                      style: const ButtonStyle(
-                        backgroundColor: WidgetStatePropertyAll(Colors.blue),
-                      ),
-                      onPressed: () {
-                        // Acción del botón 1
-                        Navigator.of(context).pushNamed('/record');
-                      },
-                      child: const Text(
-                        "CAMARA",
-                        style: TextStyle(
-                          color: Colors.white, 
-                          fontSize: 24,
-                          fontFamily: 'NerkoOne'
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/guide');
+                          },
+                          icon: Container(
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              //color: Colors.green, // Fondo del círculo
+                              border: Border.all(
+                                  color: AppColors.primary, width: 2), // Borde verde
+                            ),
+                            padding: const EdgeInsets.all(8),
+                            child: Icon(
+                              Icons.arrow_back, // Icono de flecha
+                              color: AppColors.primary, // Color del ícono
+                            ), /*const Icon(Icons.arrow_back, color: AppColors.primary, size: 20,)),*/
                           ),
-                      ),
+                        ),
+                      ],
                     ),
-                    ElevatedButton(
-                      style: const ButtonStyle(
-                        backgroundColor:
-                            WidgetStatePropertyAll(Colors.redAccent),
-                      ),
-                      onPressed: () {
-                        // Acción del botón 2
-                        
-                      Navigator.of(context).pushNamed('/chat');
-                      },
-                      child: const Text("CHAT",style: TextStyle(
-                          color: Colors.white, 
-                          fontSize: 24,
-                          fontFamily: 'NerkoOne'
-                          ),),
+                    
+
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            maximumSize:
+                                const WidgetStatePropertyAll(Size(400, 170)),
+                            shape: WidgetStatePropertyAll(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20.0))),
+                            backgroundColor:
+                                const WidgetStatePropertyAll(Colors.blue),
+                            padding: const WidgetStatePropertyAll(
+                                EdgeInsets.fromLTRB(80,10,80,0)),
+                            
+                          ),
+                          onPressed: () {
+                            // Acción del botón 1
+                            Navigator.of(context).pushNamed('/record');
+                          },
+                          child: Column(
+                            children: [
+                              //Insert Image
+                              
+                              const Text(
+                                "CAMARA",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 36,
+                                    fontFamily: 'NerkoOne'),
+                              ),Image.asset('assets/img/camera.png',
+                                  width: 100, height: 100),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            maximumSize:
+                                const WidgetStatePropertyAll(Size(300, 170)),
+                            shape: WidgetStatePropertyAll(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20.0))),
+                            backgroundColor:
+                                const WidgetStatePropertyAll(Colors.redAccent),
+                            padding: const WidgetStatePropertyAll(
+                                EdgeInsets.fromLTRB(80,0,80,0)),
+                          ),
+                          onPressed: () {
+                            // Acción del botón 2
+
+                            Navigator.of(context).pushNamed('/chat');
+                          },
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              
+                              const Text(
+                                "CHAT",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 36,
+                                    fontFamily: 'NerkoOne'),
+                              ),Image.asset('assets/img/chat.png',
+                                  width: 110, height: 110),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 )),
